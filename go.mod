e5de82e8dd9ca1986fb6d851d6fcbd00d47e3d9d
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-28 19:45:32 +0330

    Chore: add config package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/DKeshavarz/armis
 
 go 1.24.5
+
+require github.com/joho/godotenv v1.5.1
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,22 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init(){
+	Load(".env")
+}
+func GetFromEnv(key string) string {
+	return os.Getenv(key)
+}
+
+func Load(file string){
+	err := godotenv.Load(file)
+	if err != nil {
+		log.Fatalf("Error in loading .env : %s", err)
+	}
+}
