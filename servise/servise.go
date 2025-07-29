ba9d1c3b1fcdf2973a988842311ac6fb13588682
DKeshavarz <dankeshavarz1075@gmail.com>
2025-07-29 09:57:01 +0330

    Chore: add servise folder

---
+++ servise/servise.go
@@ -0,0 +1,28 @@
+package servise
+
+import (
+	"context"
+
+	"github.com/DKeshavarz/armis/internal/storage"
+)
+
+type service struct {
+	storage storage.StorageInterface
+}
+
+func NewServise()*service{
+	st := storage.NewStorage()
+	return &service{
+		storage: st,
+	}
+}
+
+func (s *service)Put(ctx context.Context, key, value string) error{
+	return nil
+}
+func (s *service)Get(ctx context.Context, key string) (string, error){
+	return "nigga",nil
+}
+func (s *service)Delete(ctx context.Context, key string) error{
+	return nil
+}
