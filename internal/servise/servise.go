74f68f2e6dc6e4abd2983d0761d6b0203adad827
armita04 <armitafarzaei@gmail.com>
2025-08-18 17:12:27 +0330

    Refactor: change logic ready for phase3

--- internal/servise/servise.go
+++ internal/servise/servise.go
@@ -10,22 +10,23 @@ import (
 
 type service struct {
 	id string
-	isMaster bool
+	masterID string
 	storage storage.StorageInterface
 }
 
 func New()*service{
 	st := storage.New()
+	tmp_id := uuid.New().String()
 	return &service{
-		id: uuid.New().String(),
-		isMaster: true,
+		id: tmp_id,
+		masterID: tmp_id,
 		storage: st,
 	}
 }
 
 func (s *service)Put(ctx context.Context, key, value string) error{
 	log.Println(s.id, "doing put...")
-	if s.isMaster {
+	if s.masterID == s.id {
 		return s.storage.Put(ctx, key, value)
 	}
 	return nil
@@ -33,15 +34,13 @@ func (s *service)Put(ctx context.Context, key, value string) error{
 
 func (s *service)Get(ctx context.Context, key string) (string, error){
 	log.Println(s.id, "doing get...")
-	if s.isMaster {
-		return s.storage.Get(ctx, key)
-	}
-	return "",nil
+	
+	return s.storage.Get(ctx, key)
 }
 
 func (s *service)Delete(ctx context.Context, key string) error{
 	log.Println(s.id, "doing delete...")
-	if s.isMaster {
+	if s.masterID == s.id  {
 		return s.storage.Delete(ctx, key)
 	}
 	return nil
