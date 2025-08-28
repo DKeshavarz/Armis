687e711308fc322cff49de09d99a27e676dcacfc
armita04 <armitafarzaei@gmail.com>
2025-08-28 14:11:36 +0330

    chore: add gossper test to code

--- internal/cluster/interface.go
+++ internal/cluster/interface.go
@@ -1,6 +1,14 @@
 package cluster
 
-import "github.com/google/uuid"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Cluster interface {
 	ACK() []*node
@@ -36,6 +44,7 @@ func New(address string, network []string, fanOut, gossipInterval int) Cluster {
 		gossipInterval: gossipInterval,
 	}
 
+	go cluster.gossip()
 	return cluster
 }
 
@@ -48,7 +57,43 @@ func (c *cluster) JoinReply() []*node {
 	return c.network
 }
 
-// ********************************
+// **********************************
+func (c *cluster) gossip() {
+	url := "http://localhost:8000/"
+	target := "cluster/ping"
+	ticker := time.NewTicker(time.Duration(c.gossipInterval) * time.Second)
+	time.Sleep(30 * time.Second)
+	log.Println("start gossip")
+	for range 5 {
+		<-ticker.C
+		ping(url + target)
+		
+	}
+
+}
+
+type response struct {
+	Status string  `json:"status"`
+	Info   []*node `json:"info"`
+}
+
+func ping(url string) []*node {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalf("Error making GET request: %v", err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response body: %v", err)
+	}
+	var r response
+	json.Unmarshal(body, &r)
+	log.Println("get data:" , r.Status , " \n and \n", r.Info)
+	defer resp.Body.Close()
+	return r.Info
+}
 
 // *********** helpers ******************
 
